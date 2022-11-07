c42b4f3776a4f18ddee01af7864c0400c9697e40
d8ahazard <d8ahazard@gmail.com>
2022-11-07 17:11:11 -0600

    Fix 8bit adam install for windoze users.
    ALWAYS READ THE DOCS THOROUGHLY. :D

--- install.py
+++ install.py
@@ -1,5 +1,7 @@
 import shutil
-from launch import run_pip
+import sys
+
+from launch import run_pip, run
 import os
 from modules.paths import script_path
 
@@ -7,10 +9,13 @@ reqs = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.t
 run_pip(f"install -r {reqs}", "requirements for Dreambooth")
 
 if os.name == "nt":
+    python = sys.executable
     bnb_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bitsandbytes_windows")
     site_dir = os.path.join(script_path, "venv", "Lib", "site-packages", "bitsandbytes")
-    if not os.path.exists(os.path.join(site_dir, "libbitsandbytes_cpu.dll")):
-        print("Copying 8Bit Adam files for Windows.")
-        for file in os.listdir(bnb_dir):
-            fullfile = os.path.join(bnb_dir, file)
+    print("Copying 8Bit Adam files for Windows.")
+    for file in os.listdir(bnb_dir):
+        fullfile = os.path.join(bnb_dir, file)
+        if file == "main.py":
+            shutil.copy(fullfile, os.path.join(site_dir,"cuda_setup"))
+        else:
             shutil.copy(fullfile, site_dir)
