159a336a1f79588274252f0b2570bacf89e52ab4
d8ahazard <d8ahazard@gmail.com>
2022-11-07 08:58:51 -0600

    Add 8-bit Adam Requirements
    Add files for windows support of 8-Bit Adam?
    Update requirements.txt

--- install.py
+++ install.py
@@ -1,4 +1,16 @@
+import shutil
 from launch import run_pip
 import os
+from modules.paths import script_path
+
 reqs = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
 run_pip(f"install -r {reqs}", "requirements for Dreambooth")
+
+if os.name == "nt":
+    bnb_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bitsandbytes_windows")
+    site_dir = os.path.join(script_path, "venv", "Lib", "site-packages", "bitsandbytes")
+    if not os.path.exists(os.path.join(site_dir, "libbitsandbytes_cpu.dll")):
+        print("Copying 8Bit Adam files for Windows.")
+        for file in os.listdir(bnb_dir):
+            fullfile = os.path.join(bnb_dir, file)
+            shutil.copy(fullfile, site_dir)
