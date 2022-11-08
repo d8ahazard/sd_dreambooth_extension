65c404d4d3cdd471719c3a59d68638fe286bda8a
d8ahazard <d8ahazard@gmail.com>
2022-11-08 10:57:57 -0600

    Update install.py

--- install.py
+++ install.py
@@ -5,9 +5,10 @@ from launch import run_pip, run
 import os
 from modules.paths import script_path
 
+name = "Dreambooth"
 reqs = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
 python = sys.executable
-run(f'{python} -m pip install -r "{reqs}"', desc=f"Installing Dreambooth Requirements", errdesc=f"Couldn't install Dreambooth requirements.")
+run(f'{python} -m pip install -r "{reqs}"', desc=f"Installing Requirements for {name}", errdesc=f"Couldn't install {name} requirements.")
 if os.name == "nt":
     python = sys.executable
     bnb_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bitsandbytes_windows")
