2f5ed1a7dc53f53286c3fd03726b5f691507135b
d8ahazard <d8ahazard@gmail.com>
2022-11-10 09:47:44 -0600

    Update install.py

--- install.py
+++ install.py
@@ -8,7 +8,9 @@ from launch import run_pip, run
 import os
 from modules.paths import script_path
 name = "Dreambooth"
-run(f'"{sys.executable}" -m pip install -r requirements.txt', "Checking Dreambooth requirements.", "Couldn't install Dreambooth requirements.")
+req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
+print(f"loading Dreambooth reqs from {req_file}")
+run(f'"{sys.executable}" -m pip install -r "{req_file}"', "Checking Dreambooth requirements.", "Couldn't install Dreambooth requirements.")
 torch_cmd="pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url " \
           "https://download.pytorch.org/whl/cu116 "
 run(f'"{sys.executable}" -m {torch_cmd}', "Checking torch and torchvision versions", "Couldn't install torch")
