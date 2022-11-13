cb6cd10334012bb291a4f7d2617eafc17d9d972e
Sascha Maigatter <sascha@maigatter.net>
2022-11-13 18:19:59 +0100

    Added CreateModel to API

--- scripts/api.py
+++ scripts/api.py
@@ -1,3 +1,7 @@
+import base64
+import io
+import time
+import uvicorn
 from fastapi import FastAPI
 from dreambooth import conversion, dreambooth
 import gradio as gr
@@ -6,7 +10,7 @@ import modules.script_callbacks as script_callbacks
 
 
 def dreamBoothAPI(demo: gr.Blocks, app: FastAPI):
-    @app.get("/dreambooth/createModel")
+    @app.post("/dreambooth/createModel")
     async def createModel(name: str, source: str, scheduler: str):
         print("Creating new Checkpoint: " + name)
         fn = conversion.extract_checkpoint(name, source, scheduler)
