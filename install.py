4addc369004ac8f7862e79becff721cd048bd0c0
d8ahazard <d8ahazard@gmail.com>
2022-11-14 10:19:30 -0600

    Add option to skip install on launch (Offline mode)

--- install.py
+++ install.py
@@ -1,52 +1,57 @@
+import os
 import shutil
-import subprocess
 import sys
-import importlib
+
 import git
-import os
 
-from launch import run_pip, run
-import os
+from launch import run
 from modules.paths import script_path
-name = "Dreambooth"
-req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
-print(f"loading Dreambooth reqs from {req_file}")
-run(f'"{sys.executable}" -m pip install -r "{req_file}"', f"Checking {name} requirements.", f"Couldn't install {name} requirements.")
 
-torch_cmd = os.environ.get('TORCH_COMMAND', None)
-if torch_cmd is None:
-    print("WARNING: overwriting existing torch/torchvision installation!")
-    torch_cmd = "pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url " \
-          "https://download.pytorch.org/whl/cu116 "
-run(f'"{sys.executable}" -m {torch_cmd}', "Checking torch and torchvision versions", "Couldn't install torch")
+dreambooth_skip_install = os.environ.get('DREAMBOOTH_SKIP_INSTALL', False)
+if dreambooth_skip_install:
+    print("Skipping dreambooth installation.")
+else:
+    name = "Dreambooth"
+    req_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
+    print(f"loading Dreambooth reqs from {req_file}")
+    run(f'"{sys.executable}" -m pip install -r "{req_file}"', f"Checking {name} requirements.",
+        f"Couldn't install {name} requirements.")
 
-try:
-    base_dir = os.path.dirname(os.path.realpath(__file__))
-    repo = git.Repo(base_dir)
-    revision = repo.rev_parse("HEAD")
-    print(f"Dreambooth revision is {revision}")
+    torch_cmd = os.environ.get('TORCH_COMMAND', None)
+    if torch_cmd is None:
+        print("WARNING: overwriting existing torch/torchvision installation!")
+        torch_cmd = "pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url " \
+                    "https://download.pytorch.org/whl/cu116 "
+    run(f'"{sys.executable}" -m {torch_cmd}', "Checking torch and torchvision versions", "Couldn't install torch")
 
-    import diffusers
-    import torch
-    import torchvision
-    ver = diffusers.__version__
-    tver = torch.__version__
-    tvver = torchvision.__version__
-    print(f"Diffusers version is {ver}.")
-    print(f"Torch version is {tver}.")
-    print(f"Torch vision version is {tvver}.")
-except Exception as e:
-    print(f"Exception doing the things: {e}")
-    pass
+    try:
+        base_dir = os.path.dirname(os.path.realpath(__file__))
+        repo = git.Repo(base_dir)
+        revision = repo.rev_parse("HEAD")
+        print(f"Dreambooth revision is {revision}")
 
-if os.name == "nt":
-    python = sys.executable
-    bnb_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bitsandbytes_windows")
-    site_dir = os.path.join(script_path, "venv", "Lib", "site-packages", "bitsandbytes")
-    print("Copying 8Bit Adam files for Windows.")
-    for file in os.listdir(bnb_dir):
-        fullfile = os.path.join(bnb_dir, file)
-        if file == "main.py":
-            shutil.copy(fullfile, os.path.join(site_dir, "cuda_setup"))
-        else:
-            shutil.copy(fullfile, site_dir)
+        import diffusers
+        import torch
+        import torchvision
+
+        ver = diffusers.__version__
+        tver = torch.__version__
+        tvver = torchvision.__version__
+        print(f"Diffusers version is {ver}.")
+        print(f"Torch version is {tver}.")
+        print(f"Torch vision version is {tvver}.")
+    except Exception as e:
+        print(f"Exception doing the things: {e}")
+        pass
+
+    if os.name == "nt":
+        python = sys.executable
+        bnb_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bitsandbytes_windows")
+        site_dir = os.path.join(script_path, "venv", "Lib", "site-packages", "bitsandbytes")
+        print("Copying 8Bit Adam files for Windows.")
+        for file in os.listdir(bnb_dir):
+            fullfile = os.path.join(bnb_dir, file)
+            if file == "main.py":
+                shutil.copy(fullfile, os.path.join(site_dir, "cuda_setup"))
+            else:
+                shutil.copy(fullfile, site_dir)
--- webui-user-dreambooth.bat
+++ webui-user-dreambooth.bat
@@ -6,5 +6,7 @@ set VENV_DIR=
 set COMMANDLINE_ARGS=
 set "TORCH_COMMAND=pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116"
 set "REQS_FILE=.\extensions\sd_dreambooth_extension\requirements.txt"
+:: Uncomment below to skip trying to install automatically on launch.
+:: set "DREAMBOOTH_SKIP_INSTALL=True"
 
 call webui.bat
