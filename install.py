bc0dde030bc9284630f0a02f88685fead29c084a
d8ahazard <d8ahazard@gmail.com>
2022-11-14 10:33:53 -0600

    Keep logging, bnb copy routines.

--- install.py
+++ install.py
@@ -24,34 +24,34 @@ else:
                     "https://download.pytorch.org/whl/cu116 "
     run(f'"{sys.executable}" -m {torch_cmd}', "Checking torch and torchvision versions", "Couldn't install torch")
 
-    try:
-        base_dir = os.path.dirname(os.path.realpath(__file__))
-        repo = git.Repo(base_dir)
-        revision = repo.rev_parse("HEAD")
-        print(f"Dreambooth revision is {revision}")
+try:
+    base_dir = os.path.dirname(os.path.realpath(__file__))
+    repo = git.Repo(base_dir)
+    revision = repo.rev_parse("HEAD")
+    print(f"Dreambooth revision is {revision}")
 
-        import diffusers
-        import torch
-        import torchvision
+    import diffusers
+    import torch
+    import torchvision
 
-        ver = diffusers.__version__
-        tver = torch.__version__
-        tvver = torchvision.__version__
-        print(f"Diffusers version is {ver}.")
-        print(f"Torch version is {tver}.")
-        print(f"Torch vision version is {tvver}.")
-    except Exception as e:
-        print(f"Exception doing the things: {e}")
-        pass
+    ver = diffusers.__version__
+    tver = torch.__version__
+    tvver = torchvision.__version__
+    print(f"Diffusers version is {ver}.")
+    print(f"Torch version is {tver}.")
+    print(f"Torch vision version is {tvver}.")
+except Exception as e:
+    print(f"Exception doing the things: {e}")
+    pass
 
-    if os.name == "nt":
-        python = sys.executable
-        bnb_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bitsandbytes_windows")
-        site_dir = os.path.join(script_path, "venv", "Lib", "site-packages", "bitsandbytes")
-        print("Copying 8Bit Adam files for Windows.")
-        for file in os.listdir(bnb_dir):
-            fullfile = os.path.join(bnb_dir, file)
-            if file == "main.py":
-                shutil.copy(fullfile, os.path.join(site_dir, "cuda_setup"))
-            else:
-                shutil.copy(fullfile, site_dir)
+if os.name == "nt":
+    python = sys.executable
+    bnb_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bitsandbytes_windows")
+    site_dir = os.path.join(script_path, "venv", "Lib", "site-packages", "bitsandbytes")
+    print("Copying 8Bit Adam files for Windows.")
+    for file in os.listdir(bnb_dir):
+        fullfile = os.path.join(bnb_dir, file)
+        if file == "main.py":
+            shutil.copy(fullfile, os.path.join(site_dir, "cuda_setup"))
+        else:
+            shutil.copy(fullfile, site_dir)
