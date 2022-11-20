58bb667b0ac3aa048bd5a99a3bd8d1e3634eae9d
d8ahazard <d8ahazard@gmail.com>
2022-11-20 17:45:47 -0600

    Don't break things if first-time launch.

--- install.py
+++ install.py
@@ -4,14 +4,10 @@ import shutil
 import sys
 
 import git
-import diffusers
-import torch
-import torchvision
-import transformers
 from launch import run
 
 from modules.paths import script_path
-from diffusers.utils.import_utils import is_xformers_available
+
 
 dreambooth_skip_install = os.environ.get('DREAMBOOTH_SKIP_INSTALL', False)
 if not dreambooth_skip_install:
@@ -36,42 +32,47 @@ repo = git.Repo(base_dir)
 revision = repo.rev_parse("HEAD")
 print(f"Dreambooth revision is {revision}")
 
-
-
-diffusers_ver = diffusers.__version__
-diffusers_rec = "0.7.2"
-torch_ver = torch.__version__
-torch_rec = "1.12.1+cu116"
-torchvision_ver = torchvision.__version__
-torchvision_rec = "0.13.1+cu116"
-transformers_ver = transformers.__version__
-transformers_rec = "4.21.0"
-torch_flag = False
-vis_flag = False
-if os.name == "nt":
-    if torch_rec != torch_ver:
-        torch_flag = True
-    if torchvision_ver != torchvision_rec:
-        vis_flag = True
-
-has_xformers = False
 try:
-    args = sys.argv
-    print(f"Args: {args}")
-    if is_xformers_available():
-        import xformers
-        import xformers.ops
-        has_xformers = True
+    import diffusers
+    import torch
+    import torchvision
+    import transformers
+    from diffusers.utils.import_utils import is_xformers_available
+    diffusers_ver = diffusers.__version__
+    diffusers_rec = "0.7.2"
+    torch_ver = torch.__version__
+    torch_rec = "1.12.1+cu116"
+    torchvision_ver = torchvision.__version__
+    torchvision_rec = "0.13.1+cu116"
+    transformers_ver = transformers.__version__
+    transformers_rec = "4.21.0"
+    torch_flag = False
+    vis_flag = False
+    if os.name == "nt":
+        if torch_rec != torch_ver:
+            torch_flag = True
+        if torchvision_ver != torchvision_rec:
+            vis_flag = True
+
+    has_xformers = False
+    try:
+        args = sys.argv
+        print(f"Args: {args}")
+        if is_xformers_available():
+            import xformers
+            import xformers.ops
+            has_xformers = True
+    except:
+        pass
+
+    print(f"[{'*' if diffusers_rec == diffusers_ver else '!'}] Diffusers version is {diffusers_ver}.")
+    print(f"[{'*' if not torch_flag else '!'}] Torch version is {torch_ver}.")
+    print(f"[{'*' if not vis_flag else '!'}] Torch vision version is {torchvision_ver}.")
+    print(f"[{'*' if transformers_ver == transformers_rec else '!'}] Transformers version is {transformers_ver}.")
+    print(f"[{'*' if has_xformers else '-'}] Xformers")
+    print(f"")
 except:
     pass
-
-print(f"[{'*' if diffusers_rec == diffusers_ver else '!'}] Diffusers version is {diffusers_ver}.")
-print(f"[{'*' if not torch_flag else '!'}] Torch version is {torch_ver}.")
-print(f"[{'*' if not vis_flag else '!'}] Torch vision version is {torchvision_ver}.")
-print(f"[{'*' if transformers_ver == transformers_rec else '!'}] Transformers version is {transformers_ver}.")
-print(f"[{'*' if has_xformers else '-'}] Xformers")
-print(f"")
-
 # Check for "different" B&B Files and copy only if necessary
 if os.name == "nt":
     python = sys.executable
