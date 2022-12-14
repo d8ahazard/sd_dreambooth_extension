29e16c3809013b674d288b2de8dc61b70b0859de
Matt <georgefly3232@gmail.com>
2022-12-14 12:40:57 -0800

    Update compile checkpoint args

--- dreambooth/train_dreambooth.py
+++ dreambooth/train_dreambooth.py
@@ -901,7 +901,9 @@ def main(args: DreamboothConfig, memory_record, use_subdir, lora_model=None, lor
                             s_pipeline.save_pretrained(args.pretrained_model_name_or_path)
 
                             compile_checkpoint(args.model_name, half=args.half_model, use_subdir=use_subdir,
-                                               reload_models=False, lora_path=out_file, log=False)
+                                               reload_models=False, lora_path=out_file, log=False,
+                                               custom_model_name=args.custom_model_name
+                                               )
                         if args.use_ema:
                             ema_unet.restore(unet.parameters())
 
