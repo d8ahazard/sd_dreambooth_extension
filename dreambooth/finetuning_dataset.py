9a3887f0e7616a790a66442cf8a2cbba2990f19b
d8ahazard <d8ahazard@gmail.com>
2023-01-13 16:36:55 -0600

    Update finetuning_dataset.py

--- dreambooth/finetuning_dataset.py
+++ dreambooth/finetuning_dataset.py
@@ -1,8 +1,6 @@
 import os.path
 import random
-from typing import Tuple
 
-import albumentations as albu
 import cv2
 import numpy as np
 import torch.utils.data
@@ -13,52 +11,15 @@ from tqdm import tqdm
 from extensions.sd_dreambooth_extension.dreambooth import db_shared
 from extensions.sd_dreambooth_extension.dreambooth.db_shared import status
 from extensions.sd_dreambooth_extension.dreambooth.finetune_utils import closest_resolution, make_bucket_resolutions, \
-    compare_prompts, get_dim, mytqdm
+    get_dim, mytqdm
 
-
-class BucketCounter:
-    def __init__(self, starting_keys=None):
-        self.counts = {}
-        print("Initializing bucket counter!")
-        if starting_keys is not None:
-            for key in starting_keys:
-                self.counts[key] = 0
-
-    def count(self, key: Tuple[int, int]):
-        if key in self.counts:
-            self.counts[key] += 1
-        else:
-            self.counts[key] = 1
-
-    def min(self):
-        return min(self.counts.values()) if len(self.counts) else 0
-
-    def max(self):
-        return max(self.counts.values()) if len(self.counts) else 0
-
-    def get(self, key: Tuple[int, int]):
-        return self.counts[key] if key in self.counts else 0
-
-    def check_reset(self):
-        if self.max() == self.min():
-            for key in list(self.counts.keys()):
-                self.counts[key] = 0
-
-    def missing(self):
-        out = {}
-        max = self.max()
-        for key in list(self.counts.keys()):
-            if self.counts[key] < max:
-                out[key] = max - self.counts[key]
-        return out
-
-    def print(self):
-        print(f"Bucket counts: {self.counts}")
 
 class DbDataset(torch.utils.data.Dataset):
-    def __init__(self, batch_size, counter, train_img_path_captions, class_img_path_captions, tokens, tokenizer,
+    def __init__(self, batch_size, train_img_path_captions, class_img_path_captions, tokens, tokenizer,
                  resolution, prior_loss_weight, hflip, random_crop, shuffle_tokens, not_pad_tokens, debug_dataset) -> None:
         super().__init__()
+        self.batch_indices = []
+        self.batch_samples = []
         print("Init dataset!")
         # A dictionary of string/latent pairs matching image paths
         self.latents_cache = {}
@@ -66,23 +27,24 @@ class DbDataset(torch.utils.data.Dataset):
         self.caption_cache = {}
         # A dictionary of (int, int) / List[(string, string)] of resolutions and the corresponding image paths/captions
         self.train_dict = {}
-        # A dictionary of string/list[(string, string)], where the string is the instance image path, the path/captions
-        # are matching reg images
+        # A dictionary of (int, int) / List[(string, string)] of resolutions and the corresponding image paths/captions
         self.class_dict = {}
-        # A dictionary containing the number of times each bucket has been enumerated over
-        self.counter = counter
-        # A list of the currently "active" resolutions to enumerate
-        self.active_resos = []
+        # A mash-up of the class/train dicts that is perfectly fair and balanced.
+        self.sample_dict = {}
+        # This is where we just keep a list of everything for batching
+        self.sample_cache = []
+        # This is just a list of the sample names that we can use to find where in the cache an image is
+        self.sample_indices = []
         # All of the available bucket resolutions
         self.resolutions = []
-        # The currently active index in our dict of buckets
-        self.bucket_index = 0
+        # Currently active resolution
+        self.active_resolution = (0, 0)
         # The currently active image index while iterating
         self.image_index = 0
         # Total len of the dataloader
         self._length = 0
-
         self.batch_size = batch_size
+        self.batch_sampler = torch.utils.data.BatchSampler(self, batch_size, drop_last=True)
         self.train_img_path_captions = train_img_path_captions
         self.class_img_path_captions = class_img_path_captions
         self.num_train_images = len(self.train_img_path_captions)
@@ -181,61 +143,94 @@ class DbDataset(torch.utils.data.Dataset):
         # Create a list of resolutions
         bucket_resos = make_bucket_resolutions(self.resolution, min_size)
         self.train_dict = {}
-        class_dict = {}
 
-        def sort_images(img_path_captions, resos, target_dict):
+
+        def sort_images(img_path_captions, resos, target_dict, is_class_img):
             for path, cap in img_path_captions:
                 image_width, image_height = get_dim(path, self.resolution)
                 reso = closest_resolution(image_width, image_height, resos)
-                target_dict.setdefault(reso, []).append((path, cap))
+                target_dict.setdefault(reso, []).append((path, cap, is_class_img))
 
-        sort_images(self.train_img_path_captions, bucket_resos, self.train_dict)
-        sort_images(self.class_img_path_captions, bucket_resos, class_dict)
+        sort_images(self.train_img_path_captions, bucket_resos, self.train_dict, False)
+        sort_images(self.class_img_path_captions, bucket_resos, self.class_dict, True)
+
         # Enumerate by resolution, cache as needed
-        def cache_images(images, reso):
-            for img_path, cap in images:
+        def cache_images(images, reso, p_bar):
+            for img_path, cap, is_prior in images:
                 if self.cache_latents and not self.debug_dataset:
                     self.cache_latent(img_path, reso)
                 self.cache_caption(img_path, cap)
+                self.sample_indices.append(img_path)
+                self.sample_cache.append((img_path, cap, is_prior))
+                pbar.update()
 
         bucket_idx = 0
-        class_count = 0
         total_len = 0
         bucket_len = {}
         max_idx_chars = len(str(len(self.train_dict.keys())))
         p_len = self.num_class_images + self.num_train_images
-        pbar = mytqdm(total=p_len, desc="Processing images")
+        nc = self.num_class_images
+        ni = self.num_train_images
+        ti = nc + ni
         db_shared.status.job_count = p_len
         db_shared.status.job_no = 0
+        total_instances = 0
+        total_classes = 0
+        pbar = mytqdm(range(p_len), desc="Caching latents..." if self.cache_latents else "Processing images...")
         for res, train_images in self.train_dict.items():
-            self.resolutions.append(res)
-            class_len = 0
             if not train_images:
                 continue
-            class_list = class_dict.get(res, [])
-            cache_images(train_images, res)
-            for image_path, caption in train_images:
-                instance_classes = []
-                for class_path, class_caption in class_list:
-                    if compare_prompts(caption, class_caption, self.tokens):
-                        instance_classes.append((class_path, class_caption))
-                        pbar.update()
-                class_count += len(instance_classes)
-                cache_images(instance_classes, res)
-                self.class_dict[image_path] = instance_classes
-                pbar.update()
-                class_len += 1 + (1 if class_count > 0 else 0)
-            bucket_len[res] = class_len
-            total_len += class_len
+            self.resolutions.append(res)
+            cache_images(train_images, res, pbar)
+            inst_count = len(train_images)
+            class_count = 0
+            if res in self.class_dict:
+                class_images = self.class_dict[res]
+                cache_images(class_images, res, pbar)
+                class_count = len(class_images)
+            total_instances += inst_count
+            total_classes += class_count
+            example_len = inst_count if class_count == 0 else inst_count * 2
+            bucket_len[res] = example_len
+            total_len += example_len
             bucket_str = str(bucket_idx).rjust(max_idx_chars, " ")
-            inst_str = str(len(train_images)).rjust(5, " ")
-            class_str = str(class_count).rjust(5, " ")
-            ex_str = str(class_len).rjust(5, " ")
-            tqdm.write(f"Bucket {bucket_str} {res} - Instance Images: {inst_str} | Class Images: {class_str} | Examples: {ex_str}")
+            inst_str = str(len(train_images)).rjust(len(str(ni)), " ")
+            class_str = str(class_count).rjust(len(str(nc)), " ")
+            ex_str = str(example_len).rjust(len(str(ti * 2)), " ")
+            pbar.write(f"Bucket {bucket_str} {res} - Instance Images: {inst_str} | Class Images: {class_str} | Examples/batch: {ex_str}")
             bucket_idx += 1
-
-        self._length = total_len // self.batch_size
+        bucket_str = str(bucket_idx).rjust(max_idx_chars, " ")
+        inst_str = str(total_instances).rjust(len(str(ni)), " ")
+        class_str = str(total_classes).rjust(len(str(nc)), " ")
+        tot_str = str(total_len).rjust(len(str(ti)), " ")
+        pbar.write(f"Totals: Buckets {bucket_str} | Instance Images: {inst_str} | Class Images: {class_str} | Examples/batch: {tot_str}")
+        self._length = len(self.train_dict.items()) * self.batch_size
+        self._length = total_len
         print(f"Total images / batch: {self._length}, total examples: {total_len}")
+
+    def shuffle_buckets(self):
+        sample_dict = {}
+        batch_indices = []
+        batch_samples = []
+        for key in self.train_dict.keys():
+            sample_list = []
+            random.shuffle(self.train_dict[key])
+            for entry in self.train_dict[key]:
+                sample_list.append(entry)
+                batch_indices.append(entry[0])
+                batch_samples.append(entry)
+                if key in self.class_dict:
+                    class_entries = self.class_dict[key]
+                    selection = random.choice(class_entries)
+                    batch_indices.append(selection[0])
+                    batch_samples.append(selection)
+                    sample_list.append(selection)
+            sample_dict[key] = sample_list
+        if self.debug_dataset:
+            print(f"Shuffled bucket keys: {sample_dict.keys()}")
+        self.sample_dict = sample_dict
+        self.batch_indices = batch_indices
+        self.batch_samples = batch_samples
 
     def check_shuffle_tokens(self, caption):
         if self.shuffle_tokens:
@@ -247,109 +242,47 @@ class DbDataset(torch.utils.data.Dataset):
             caption = ','.join(tags)
         return caption
 
-    def set_buckets(self):
-        # Initialize list of bucket counts if not set
-        all_resos = self.resolutions
-
-        # Enumerate each bucket, adding those which need to be leveled
-        pop_index = 0
-        resos_to_use = []
-        am = self.counter.missing()
-        missing = am.copy()
-        pop_index = 0
-        #self.counter.check_reset()
-        if len(missing):
-            while len(resos_to_use) < len(all_resos):
-                if len(missing):
-                    for res, count in am.items():
-                        resos_to_use.append(res)
-                        missing[res] -= 1
-                        if missing[res] == 0:
-                            del missing[res]
-                        am = missing.copy()
-                else:
-                    resos_to_use.append(all_resos[pop_index])
-                    pop_index += 1
-                    if pop_index >= len(all_resos):
-                        pop_index = 0
-        else:
-            resos_to_use = all_resos.copy()
-        random.shuffle(resos_to_use)
-        self.active_resos = resos_to_use
-        self.bucket_index = 0
-        self.image_index = 0
-
     def __len__(self):
         return self._length
 
-    def __getitem__(self, index):
-        # Reset bucket counts if we're starting from the start of the dataloader
-        if index == 0:
-            self.set_buckets()
+    def get_example(self, res):
         # Select the current bucket of image paths
-        if self.bucket_index >= len(self.active_resos):
-            self.bucket_index = 0
-        bucket_res = self.active_resos[self.bucket_index]
-        bucket = self.train_dict[bucket_res]
-        # Initialize outputs
-        image_paths = []
-        images = [None] * self.batch_size
-        ids = [None] * self.batch_size
-        loss_weights = torch.empty(self.batch_size, dtype=torch.float32)
+        bucket = self.sample_dict[res]
 
         # Set start position from last iteration
         img_index = self.image_index
 
-        count = 0
-        # Loop the current bucket until we fill our batch
-        bucket_emptied = False
-        repeats = 0
-        while count < self.batch_size:
-            # Grab instance image data
-            image_path, caption = bucket[img_index]
-            image_data, input_ids = self.load_image(image_path, caption, bucket_res)
-            if self.debug_dataset:
-                image_paths.append(image_path)
-            images[count] = image_data
-            ids[count] = input_ids
-            loss_weights[count] = 1.0
-            count += 1
-            if count < self.batch_size:
-                class_image_paths = self.class_dict[image_path]
-                # Select a random class image from our instance image's available class image.
-                if len(class_image_paths):
-                    class_image_path, class_caption = random.choice(class_image_paths)
-                    if class_image_path is not None:
-                        # Grab class image data
-                        image_data, input_ids = self.load_image(class_image_path, class_caption, bucket_res)
-                        images[count] = image_data
-                        ids[count] = input_ids
-                        loss_weights[count] = self.prior_loss_weight
-                        count += 1
+        # Reset image index (double-check)
+        if img_index >= len(bucket):
+            img_index = 0
 
-            img_index += 1
-            if img_index >= len(bucket):
-                bucket_emptied = True
-                img_index = 0
-                repeats += 1
-                self.counter.count(bucket_res)
+        repeats = 0
+        # Grab instance image data
+        image_path, caption, is_class_image = bucket[img_index]
+        image_index = self.sample_indices.index(image_path)
+
+        img_index += 1
+
+        # Reset image index
+        if img_index >= len(bucket):
+            img_index = 0
+            repeats += 1
 
 
         self.image_index = img_index
-        # If we have reached the end of our bucket, increment to the next, update the count, reset image index.
-        if bucket_emptied:
-            # print(f"Incrementing bucket {self.bucket_index}-{bucket_res}, {self.counter.get(bucket_res)}")
-            self.bucket_index += 1
-            self.image_index = 0
 
-        # Stack and return outputs
+        return image_index, repeats
+
+    def __getitem__(self, index):
+        image_path, caption, is_class_image = self.sample_cache[index]
         if not self.debug_dataset:
-            images = torch.stack(images)
-            ids = torch.cat(ids, dim=0)
-            if not self.cache_latents:
-                images = images.to(memory_format=torch.contiguous_format)
-
-        example = {"images": images, "input_ids": ids, "loss_weight": loss_weights, "res": bucket_res}
+            image_data, input_id = self.load_image(image_path, caption, self.active_resolution)
+        else:
+            image_data = image_path
+            input_id = caption
+        loss_weight = self.prior_loss_weight if is_class_image else 1.0
+        # If we have reached the end of our bucket, increment to the next, update the count, reset image index.
+        example = {"image": image_data, "input_id": input_id, "loss_weight": loss_weight, "res": self.active_resolution}
         return example
 
 
