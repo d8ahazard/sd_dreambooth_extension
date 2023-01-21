4692019176d465ea4f952a6abf74d9939533c675
Matt <georgefly3232@gmail.com>
2023-01-20 16:38:44 -0800

    Use patch pipe for generating samples.

--- dreambooth/finetune_utils.py
+++ dreambooth/finetune_utils.py
@@ -26,7 +26,7 @@ from extensions.sd_dreambooth_extension.dreambooth.db_config import DreamboothCo
 from extensions.sd_dreambooth_extension.dreambooth.db_shared import status
 from extensions.sd_dreambooth_extension.dreambooth.prompt_data import PromptData
 from extensions.sd_dreambooth_extension.dreambooth.utils import cleanup, get_checkpoint_match, get_images, db_save_image
-from extensions.sd_dreambooth_extension.lora_diffusion.lora import apply_lora_weights
+from extensions.sd_dreambooth_extension.lora_diffusion.lora import patch_pipe
 from modules import shared, devices, sd_models, sd_hijack, prompt_parser, lowvram
 from modules.processing import StableDiffusionProcessingTxt2Img, StableDiffusionProcessing, Processed, \
     get_fixed_seed, create_infotext, decode_first_stage
@@ -463,7 +463,11 @@ class ImageBuilder:
                 if no_safe:
                     db_shared.start_safe_unpickle()
             if config.use_lora and lora_model is not None and lora_model != "":
-                apply_lora_weights(self.image_pipe.unet, self.image_pipe.text_encoder, config, is_ui=True)
+                patch_pipe(
+                    pipe=self.image_pipe,
+                    unet=config.lora_model,
+                    r=config.lora_rank
+                )
         else:
             current_model = sd_models.select_checkpoint()
             new_model_info = get_checkpoint_match(config.src)
