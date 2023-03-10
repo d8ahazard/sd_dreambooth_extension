9ef77e39bada890c963aa20a484c6a259544204f
ArrowM <arrowman.6677@gmail.com>
2023-03-10 14:58:56 -0600

    uncomment growth rate

--- dreambooth/train_dreambooth.py
+++ dreambooth/train_dreambooth.py
@@ -455,7 +455,7 @@ def main(class_gen_method: str = "Native Diffusers") -> TrainResult:
                     lr=args.learning_rate,
                     momentum=args.adaptation_momentum,
                     weight_decay=args.adamw_weight_decay,
-                    # growth_rate=args.adaptation_growth_rate,
+                    growth_rate=args.adaptation_growth_rate,
                 )
 
             else:
