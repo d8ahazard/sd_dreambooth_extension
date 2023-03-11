c7f3483dc8900a681e15da0620d87dd32eae17c7
saunderez <saunderez@gmail.com>
2023-03-11 11:16:26 +1000

    Could've told me about that before the last commit....

--- scripts/main.py
+++ scripts/main.py
@@ -1454,7 +1454,7 @@ def on_ui_tabs():
             )
 
         def optimizer_changed(opti):
-            show_adapt = opti in ["SGD Dadaptation", "AdaGrad Dadaptation", "AdamW Dadaptation", "Adan Dadaptation""]
+            show_adapt = opti in ["SGD Dadaptation", "AdaGrad Dadaptation", "AdamW Dadaptation", "Adan Dadaptation"]
             adaptation_lr = gr.update(visible=show_adapt)
             return adaptation_lr
 
