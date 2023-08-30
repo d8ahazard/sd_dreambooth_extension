3b05f0d6ce06935c62d3b7ad30e1e81de9ea4001
d8ahazard <d8ahazard@gmail.com>
2023-08-29 20:26:16 -0500

    Update shared.py

--- dreambooth/shared.py
+++ dreambooth/shared.py
@@ -43,7 +43,6 @@ def load_auto_settings():
         lowvram = ws.cmd_opts.lowvram
         config = ws.cmd_opts.config
         device = ws.device
-        sd_model = ws.sd_model
         in_progress = False
         in_progress_epoch = 0
         in_progress_step = 0
