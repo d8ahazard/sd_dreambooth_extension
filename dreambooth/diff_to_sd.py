e27f707a296e69701b991d802081ad38e2b78067
d8ahazard <d8ahazard@gmail.com>
2023-11-15 09:02:45 -0600

    Manually implement PR #1359
    Thanks @Imageder for the contribution!

--- dreambooth/diff_to_sd.py
+++ dreambooth/diff_to_sd.py
@@ -20,7 +20,7 @@ from dreambooth.dataclasses.db_config import from_file, DreamboothConfig
 from dreambooth.shared import status
 from dreambooth.utils.model_utils import unload_system_models, \
     reload_system_models, \
-    disable_safe_unpickle, enable_safe_unpickle, import_model_class_from_model_name_or_path
+    safe_unpickle_disabled, import_model_class_from_model_name_or_path
 from dreambooth.utils.utils import printi
 from helpers.mytqdm import mytqdm
 from lora_diffusion.lora import merge_lora_to_model
@@ -562,9 +562,8 @@ def load_model(model_path: str, map_location: str):
     if ".safetensors" in model_path:
         return safetensors.torch.load_file(model_path, device=map_location)
     else:
-        disable_safe_unpickle()
-        loaded = torch.load(model_path, map_location=map_location)
-        enable_safe_unpickle()
+        with safe_unpickle_disabled():
+            loaded = torch.load(model_path, map_location=map_location)
         return loaded
 
 
