54bcbe7c3038b8caa2875402fb1f17d5d2a194ad
Anzhc <133806049+Anzhc@users.noreply.github.com>
2024-03-08 05:17:12 +0200

    Add stuff for training.
    functions for snr, multires noise, preparing scheduler for min snr and in future zero snr. Mostly taken from Kohya.

--- dreambooth/deis_velocity.py
+++ dreambooth/deis_velocity.py
@@ -1,5 +1,6 @@
 import torch
 from diffusers.schedulers import DEISMultistepScheduler
+import random
 
 def get_velocity(
         self, sample: torch.FloatTensor, noise: torch.FloatTensor, timesteps: torch.IntTensor
@@ -21,28 +22,30 @@ def get_velocity(
     velocity = sqrt_alpha_prod * noise - sqrt_one_minus_alpha_prod * sample
     return velocity
 
-def compute_snr(timesteps: torch.IntTensor):
+def compute_snr(timesteps: torch.IntTensor, noise_scheduler):
     """
     Computes SNR as per https://github.com/TiankaiHang/Min-SNR-Diffusion-Training/blob/521b624bd70c67cee4bdf49225915f5945a872e3/guided_diffusion/gaussian_diffusion.py#L847-L849
     """
-    noise_scheduler = DEISMultistepScheduler(
-        num_train_timesteps=1000,
-        beta_start=0.0001,
-        beta_end=0.02,
-        beta_schedule="linear",
-        trained_betas=None,
-        solver_order=2,
-        prediction_type="epsilon",
-        thresholding=False,
-        dynamic_thresholding_ratio=0.995,
-        sample_max_value=1,
-        algorithm_type="deis",
-        solver_type="logrho",
-        lower_order_final=True,
-        use_karras_sigmas=False,
-        timestep_spacing="linspace",
-        steps_offset=0
-    )
+#    noise_scheduler = DEISMultistepScheduler(
+#        num_train_timesteps=1000,
+#        beta_start=0.0001,
+#        beta_end=0.02,
+#        beta_schedule="linear",
+#        trained_betas=None,
+#        solver_order=2,
+#        prediction_type="epsilon",
+#        thresholding=False,
+#        dynamic_thresholding_ratio=0.995,
+#        sample_max_value=1,
+#        algorithm_type="deis",
+#        solver_type="logrho",
+#        lower_order_final=True,
+#        use_karras_sigmas=False,
+#        timestep_spacing="linspace",
+#        steps_offset=0
+#    )
+#Not needed, just pass noise scheduler, idk
+    noise_scheduler=noise_scheduler
     alphas_cumprod = noise_scheduler.alphas_cumprod
     sqrt_alphas_cumprod = alphas_cumprod**0.5
     sqrt_one_minus_alphas_cumprod = (1.0 - alphas_cumprod) ** 0.5
@@ -61,3 +64,76 @@ def compute_snr(timesteps: torch.IntTensor):
     # Compute SNR.
     snr = (alpha / sigma) ** 2
     return snr
+
+def fix_noise_scheduler_betas_for_zero_terminal_snr(noise_scheduler):
+    # fix beta: zero terminal SNR
+    # taken from Kohya
+    print(f"fix noise scheduler betas: https://arxiv.org/abs/2305.08891")
+
+    def enforce_zero_terminal_snr(betas):
+        # Convert betas to alphas_bar_sqrt
+        alphas = 1 - betas
+        alphas_bar = alphas.cumprod(0)
+        alphas_bar_sqrt = alphas_bar.sqrt()
+
+        # Store old values.
+        alphas_bar_sqrt_0 = alphas_bar_sqrt[0].clone()
+        alphas_bar_sqrt_T = alphas_bar_sqrt[-1].clone()
+        # Shift so last timestep is zero.
+        alphas_bar_sqrt -= alphas_bar_sqrt_T
+        # Scale so first timestep is back to old value.
+        alphas_bar_sqrt *= alphas_bar_sqrt_0 / (alphas_bar_sqrt_0 - alphas_bar_sqrt_T)
+
+        # Convert alphas_bar_sqrt to betas
+        alphas_bar = alphas_bar_sqrt**2
+        alphas = alphas_bar[1:] / alphas_bar[:-1]
+        alphas = torch.cat([alphas_bar[0:1], alphas])
+        betas = 1 - alphas
+        return betas
+
+    betas = noise_scheduler.betas
+    betas = enforce_zero_terminal_snr(betas)
+    alphas = 1.0 - betas
+    alphas_cumprod = torch.cumprod(alphas, dim=0)
+
+    # print("original:", noise_scheduler.betas)
+    # print("fixed:", betas)
+
+    noise_scheduler.betas = betas
+    noise_scheduler.alphas = alphas
+    noise_scheduler.alphas_cumprod = alphas_cumprod
+
+def apply_debiased_estimation(loss, timesteps, noise_scheduler):
+    #Debias, taken from Kohya
+    snr_t = torch.stack([noise_scheduler.all_snr[t] for t in timesteps])  # batch_size
+    snr_t = torch.minimum(snr_t, torch.ones_like(snr_t) * 1000)  # if timestep is 0, snr_t is inf, so limit it to 1000
+    weight = 1/torch.sqrt(snr_t)
+    loss = weight * loss
+    #loss = loss.mean()
+    return loss
+
+# https://wandb.ai/johnowhitaker/multires_noise/reports/Multi-Resolution-Noise-for-Diffusion-Model-Training--VmlldzozNjYyOTU2
+def pyramid_noise_like(noise, device, iterations=6, discount=0.4):
+    b, c, w, h = noise.shape  # EDIT: w and h get over-written, rename for a different variant!
+    u = torch.nn.Upsample(size=(w, h), mode="bilinear").to(device)
+    for i in range(iterations):
+        r = random.random() * 2 + 2  # Rather than always going 2x,
+        wn, hn = max(1, int(w / (r**i))), max(1, int(h / (r**i)))
+        noise += u(torch.randn(b, c, wn, hn).to(device)) * discount**i
+        if wn == 1 or hn == 1:
+            break  # Lowest resolution is 1x1
+    return noise / noise.std()  # Scaled back to roughly unit variance
+
+def prepare_scheduler_for_custom_training(noise_scheduler, device):
+    #taken from kohya
+    if hasattr(noise_scheduler, "all_snr"):
+        return
+
+    alphas_cumprod = noise_scheduler.alphas_cumprod
+    sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod)
+    sqrt_one_minus_alphas_cumprod = torch.sqrt(1.0 - alphas_cumprod)
+    alpha = sqrt_alphas_cumprod
+    sigma = sqrt_one_minus_alphas_cumprod
+    all_snr = (alpha / sigma) ** 2
+
+    noise_scheduler.all_snr = all_snr.to(device)
